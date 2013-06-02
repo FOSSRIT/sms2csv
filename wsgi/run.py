232f2482a6c3db9fd49aa2428abc96d938602f5f
RemyD <remyd@civx.us>
2013-06-02 12:51:40 -0400

    hopefully fix 500 error

--- wsgi/run.py
+++ wsgi/run.py
@@ -10,13 +10,18 @@ def smsResponse():
 
 @app.route("/incoming", methods=['GET', 'POST'])
 def receiveResponse():
-    data_dir = os.environ.get("OPENSHIFT_DATA_DIR")
+    data_dir = os.environ.get("OPENSHIFT_DATA_DIR", '.')
     with open(os.path.join(data_dir, 'tempfile'), 'a') as log:
 
         body = request.args.get('body')
         if not body:
             body = request.args.keys()
-        log.write(body)
+        if not body:
+            body = request.args
+        if not body:
+            "Well I don't know then"
+        log.write(str(body))
+    return str(body)
 
 if __name__ == "__main__":
     app.run(debug=True)
