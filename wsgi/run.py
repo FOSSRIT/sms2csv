c528329d6a0d1b25f9b0333f205172e3dc45142c
RemyD <remyd@civx.us>
2013-06-02 13:04:23 -0400

    Add default for from number

--- wsgi/run.py
+++ wsgi/run.py
@@ -12,7 +12,7 @@ def smsResponse():
 def receiveResponse():
     data_dir = os.environ.get("OPENSHIFT_DATA_DIR", '.')
     body = request.args.get('Body')
-    from_number = request.args.get('From')
+    from_number = request.args.get('From', '1111111')
     if not body or from_number:
         return "Please don't go here manually."
     with open(os.path.join(data_dir, from_number), 'a') as log:
