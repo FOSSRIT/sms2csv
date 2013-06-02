6249b79fe840134a36ab4feb7cba7c05134f9cd3
RemyD <remyd@civx.us>
2013-06-02 12:24:17 -0400

    Add /incoming route

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,9 +1,20 @@
-from flask import Flask
+from flask import Flask, request, redirect
+import twilio.twiml
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello OpenShift World!"
+@app.route("/", methods=['GET', 'POST'])
+def smsResponse():
+    """Respond to incoming calls with a simple text message."""
+    resp = twilio.twiml.Response()
+    resp.sms("Your, text has been received")
+    return str(resp)
+
+@app.route("/incoming", methods=['GET', 'POST'])
+def receiveResponse():
+    body = request.args.get('body')
+    if not body:
+        print request.args.keys()
+    return body
 
 if __name__ == "__main__":
     app.run(debug=True)
