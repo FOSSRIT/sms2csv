9d159d54662c5b7fd775159f32eb9467105c8afa
RemyD <remyd@civx.us>
2013-06-02 12:42:54 -0400

    Write SMSs to a file, not render, because that does not do a thing

--- wsgi/run.py
+++ wsgi/run.py
@@ -1,20 +1,22 @@
 from flask import Flask, request, redirect
-import twilio.twiml
+import os
 app = Flask(__name__)
 
 @app.route("/", methods=['GET', 'POST'])
 def smsResponse():
     """Respond to incoming calls with a simple text message."""
-    resp = twilio.twiml.Response()
-    resp.sms("Your, text has been received")
-    return str(resp)
+    resp = "Your, text has been received"
+    return resp
 
 @app.route("/incoming", methods=['GET', 'POST'])
 def receiveResponse():
-    body = request.args.get('body')
-    if not body:
-        print request.args.keys()
-    return body
+    data_dir = os.environ.get("OPENSHIFT_DATA_DIR")
+    with open(os.path.join(data_dir, 'tempfile'), 'a') as log:
+
+        body = request.args.get('body')
+        if not body:
+            body = request.args.keys()
+        log.write(body)
 
 if __name__ == "__main__":
     app.run(debug=True)
